4b1ee362bb6b28a2a7aa45c458a9de150a2edebf
John Pankowicz <johnpankowicz@hotmail.com>
2020-01-14 13:11:49 -0500

    Update program.cs with prior contents

--- BackEnd/Web_App/Program.cs
+++ BackEnd/Web_App/Program.cs
@@ -1,3 +1,17 @@
+//using System;
+//using System.IO;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore;
+////using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
+//using NLog;
+//using NLog.Web;
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +21,31 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using System.IO;
+using NLog;
+using NLog.Web;
+
+
 namespace GM.WebApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            // NLog: setup the logger first to catch all errors
+            var logger = LogManager.LoadConfiguration("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                //BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +54,27 @@ namespace GM.WebApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureAppConfiguration((hostingContext, config) => 
+                {
+
+                    var env = hostingContext.HostingEnvironment;
+                    if (env.IsDevelopment())
+                    {
+                        // If development, include _SECRETS/appsettings.Development.json in the configuration.
+                        // This file contains the keys for using reCaptcha and Google external authorization.
+                        string devSettingFile = Environment.CurrentDirectory + @"\..\..\..\..\_SECRETS\" + $"appsettings.{env.EnvironmentName}.json";
+                        if (File.Exists(devSettingFile))
+                        {
+                            config.AddJsonFile(devSettingFile, optional: true, reloadOnChange: true);
+                        }
+                    }
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog();  // NLog: setup NLog for Dependency injection
     }
 }
