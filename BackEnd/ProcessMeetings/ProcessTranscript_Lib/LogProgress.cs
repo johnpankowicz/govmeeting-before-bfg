35e8a7370266e4ae7e5425669c5f1316c57c9b61
John Pankowicz <johnpankowicz@hotmail.com>
2020-04-01 22:34:30 -0400

    Move logging from Specific_XXX base class to external class

---
+++ BackEnd/ProcessMeetings/ProcessTranscript_Lib/LogProgress.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ProcessTranscript_Lib
+{
+    class LogProgress
+    {
+        string officersNames;
+        string meetingInfo;
+        //protected string meetingDate;
+        string logDirectory;
+
+        int step = 1;
+
+        public LogProgress(string _logDirectory)
+        {
+            logDirectory = _logDirectory;
+        }
+
+        public void SetParts(string info, string names)
+        {
+            meetingInfo = info;
+            officersNames = names;
+        }
+
+        public void Log(string fix_step, string transcriptText)
+        {
+            string outputFile = logDirectory + "\\" + "2-" + step + " " + fix_step + ".txt";
+            step++;
+
+            File.WriteAllText(outputFile, meetingInfo + "-----------------------------\n" + officersNames + "-----------------------------\n" + transcriptText);
+        }
+    }
+}
